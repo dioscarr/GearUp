98b73295b1b8b9134cb06b3cc4ce2cf2719b7776
dioscarr <Dioscarr@gmail.com>
2016-04-10 10:48:31 -0400

    init

--- GearUp/Migrations/Configuration.cs
+++ GearUp/Migrations/Configuration.cs
@@ -10,6 +10,7 @@ namespace GearUp.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(GearUp.Models.MyDbContext context)
